42b61283c18ea9be5049b47a682dbea11f6f9a2e
Tahseen05 <shaikhtehsin05@gmail.com>
2022-04-10 17:19:27 +0530

    Some minor change

--- DemoFrameWork/src/test/java/apiFramework/GetApi2.java
+++ DemoFrameWork/src/test/java/apiFramework/GetApi2.java
@@ -19,23 +19,14 @@ public class GetApi2
 			{
 				// to specify base uri
 //				RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
-//				https://api.citygridmedia.com/content/places/v2/search/where?what=hotels&where=boston,ma&page=1&rpp=5&sort=alpha&publisher=test&format=json
 				RestAssured.baseURI = "https://bookstore.toolsqa.com/BookStore/v1";
-//				RestAssured.baseURI = "https://api.citygridmedia.com/content/places/v2";
+
 				
 				// Request Object
 				RequestSpecification ga = RestAssured.given();
 				
 				//Response Object
-//				Response response = ga.request(Method.GET ,"/search/where?what=hotels&where=boston,ma&page=1&rpp=5&sort=alpha&publisher=test&format=json");
-//				
-//				ga.params("what", "hotels");
-//				ga.params("where", "boston,ma");
-//				ga.params("page", "1");
-//				ga.params("rpp", "5");
-//				ga.params("sort", "alpha");
-//				ga.params("publisher", "test");
-//				ga.params("format", "json");
+
 				
 				Response response = ga.queryParam ("ISBN","9781449325862").get("/Book");
 
